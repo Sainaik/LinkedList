f5e97e43af934e20672deac9eaba3761e31333cf
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-21 22:08:13 +0530

    [Sai]First Commit

---
+++ LinkedList_DS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedList_DS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List Data Structure!");
+        }
+    }
+}
