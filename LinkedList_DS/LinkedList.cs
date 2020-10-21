4583d2f451f574fc9a29ab63460a3be295a0a4f9
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-21 22:16:03 +0530

    [Sai]Added. Node class and Created a LL

---
+++ LinkedList_DS/LinkedList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList_DS
+{
+    public class LinkedList
+    {
+        internal Node head;
+
+        internal LinkedList()
+        {
+            this.head = null;
+
+        }
+
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+
+
+            if (this.head == null)
+            {
+                this.head = node;
+
+            }
+            else
+            {
+                Node temp = head;
+
+                while (temp.next != null)
+                {
+
+                    temp = temp.next;
+                }
+
+                temp.next = node;
+
+
+            }
+            Console.WriteLine($"Data {node.data} is added to LinkedList");
+        }
+
+
+        internal void Display()
+        {
+            Console.WriteLine("\nLinked List Elements:");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("List is empty");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine($"Data {temp.data}");
+                    temp = temp.next;
+                }
+            }
+
+            Console.WriteLine("\n");
+
+        }
+
+    }
+}
--- LinkedList_DS/Program.cs
+++ LinkedList_DS/Program.cs
@@ -7,6 +7,18 @@ namespace LinkedList_DS
         static void Main(string[] args)
         {
             Console.WriteLine("Linked List Data Structure!");
+
+            LinkedList linkedList = new LinkedList();
+
+            linkedList.Add(56);
+
+
+            linkedList.Add(30);
+
+
+            linkedList.Add(70);
+
+            linkedList.Display();
         }
     }
 }
